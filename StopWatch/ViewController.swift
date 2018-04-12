0c52f85d0f6aafbd9f54e9ea2358df6e5dae793c
D7703_15 <d7703_15@D7703-15ui-iMac.local>
2018-04-12 11:20:12 +0900

    StopWatch

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startBt: UIButton!
+    @IBOutlet weak var stopBt: UIButton!
+    @IBOutlet weak var resetBt: UIButton!
+    
+    var count = 0
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startBt.layer.cornerRadius = 0.2 * startBt.bounds.size.width
+        stopBt.layer.cornerRadius = 0.2 * stopBt.bounds.size.width
+        resetBt.layer.cornerRadius = 0.2 * resetBt.bounds.size.width
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func start(_ sender: Any) {
+    myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()})
+        changeBt(start: false, stop: true, reset: true)
     }
-
-
+    
+    @IBAction func stop(_ sender: Any) {
+    myTimer.invalidate()
+        changeBt(start: true, stop: false, reset: true)
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+        timeLabel.text = "00:00:00"
+        changeBt(start: true, stop: true, reset: false)
+    }
+    
+    func updateTime(){
+    count = count + 1
+    let min = count / 60 / 100
+    let sec = (count - (min * 60 * 100)) / 100
+    let msec = count - (min * 60 * 100) - (sec * 100)
+    timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
 }
-
+    func changeBt(start: Bool, stop: Bool, reset: Bool){
+        startBt.isEnabled = start
+        stopBt.isEnabled = stop
+        resetBt.isEnabled = reset
+    }
+}
